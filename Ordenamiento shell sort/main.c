fd0d223015e73035132c9df7925288819d04c1b3
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-01-09 09:56:35 -0500

    Ordenamiento Shell Sort v1.0

---
+++ Ordenamiento shell sort/main.c
@@ -0,0 +1,51 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void ordenamiento_shell_sort(int* arr, size_t n);
+void mostrar(int *arr, size_t n);
+
+int main()
+{
+    size_t N = 8;
+    int arr[] = {6, 5, 3, 1, 8, 7, 2, 4};
+    puts("Array desordenado =>");
+    mostrar(arr, N);
+    ordenamiento_shell_sort(arr, N);
+    puts("\nArray ordenado =>");
+    mostrar(arr, N);
+
+    return EXIT_SUCCESS;
+}
+
+void ordenamiento_shell_sort(int *arr, size_t n)
+{
+    int aux, k;
+
+    for (size_t rzn = n / 2; rzn > 0; rzn /= 2)
+    {
+        for (size_t i = rzn; i < n; i++)
+        {
+            aux = arr[i];
+            k = i - rzn;
+
+            while (k >= 0 && aux < arr[k])
+            {
+                arr[k + rzn] = arr[k];
+                k -= rzn;
+            }
+
+            arr[k + rzn] = aux;
+        }
+    }
+}
+
+void mostrar(int *arr, size_t n)
+{
+    printf("[ ");
+
+    for (size_t i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    puts("]");
+}
